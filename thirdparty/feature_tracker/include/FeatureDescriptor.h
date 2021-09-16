14b0fc76c2f19d0c902039e422763122b3efc755
lacie-life <00sao00ios00@gmail.com>
2021-09-16 21:43:27 +0700

    [Add] added superpoint feature test

---
+++ thirdparty/feature_tracker/include/FeatureDescriptor.h
@@ -0,0 +1,125 @@
+#ifndef FEATURE_DESCRIPTOR_H
+#define FEATURE_DESCRIPTOR_H
+
+#include <glog/logging.h>
+#include <Eigen/Core>
+#include <algorithm>
+#include <random>
+#include <vector>
+
+static const int kDefaultReservedSpace = 2000;
+
+template <typename ScalarType>
+class FeatureDescriptor
+{
+public:
+    FeatureDescriptor() : num_features_(0), descriptor_size_(0) {}
+
+    FeatureDescriptor(int descriptor_size,
+                      int reserved_space = kDefaultReservedSpace)
+        : num_features_(0), descriptor_size_(descriptor_size)
+    {
+        descriptor_data_.reserve(reserved_space * descriptor_size_);
+    }
+
+    // Set the number of bytes per descriptor. Any existing data is invalidated.
+    // The number of features is always zero following this call; the
+    // reserved_space argument only reserves space so that future resize() calls
+    // avoid reallocation.
+    void Configure(int descriptor_size,
+                   int reserved_space = kDefaultReservedSpace)
+    {
+        descriptor_size_ = descriptor_size;
+        Resize(0);
+        descriptor_data_.reserve(reserved_space * descriptor_size_);
+    }
+
+    // Change the number of features. If n > num_features_ then this function
+    // may trigger a re-allocation. If n < num_features_ then this function
+    // never re-allocates. In either case the previous data is always retained.
+    void Resize(int n)
+    {
+        num_features_ = n;
+        descriptor_data_.resize(n * descriptor_size_);
+    }
+
+    void Clear()
+    {
+        num_features_ = 0;
+        descriptor_data_.clear();
+    }
+
+    inline uint32_t Size() const { return num_features_; }
+
+    inline bool Empty() const { return num_features_ == 0; }
+
+    inline uint32_t DescriptorSize() const { return descriptor_size_; }
+
+    inline ScalarType *descriptor(uint32_t index)
+    {
+        return &descriptor_data_[index * descriptor_size_];
+    }
+
+    inline const ScalarType *descriptor(uint32_t index) const
+    {
+        return &descriptor_data_[index * descriptor_size_];
+    }
+
+    inline void SetDescriptorToZero(uint32_t index)
+    {
+        memset(descriptor(index), 0, descriptor_size_);
+    }
+
+    inline void SetDescriptorRandom(uint32_t index, int seed)
+    {
+        std::mt19937 generator(seed);
+        ScalarType *data = descriptor(index);
+        for (size_t i = 0; i < descriptor_size_; ++i)
+        {
+            data[i] = generator() % 256;
+        }
+    }
+
+    inline ScalarType *descriptor_data() { return descriptor_data_.data(); }
+    inline const ScalarType *descriptor_data() const
+    {
+        return descriptor_data_.data();
+    }
+
+    void Swap(FeatureDescriptor *other)
+    {
+        CHECK_NE(other, this);
+        CHECK_NOTNULL(other);
+        std::swap(num_features_, other->num_features_);
+        std::swap(descriptor_size_, other->descriptor_size_);
+        std::swap(descriptor_data_, other->descriptor_data_);
+    }
+
+    // Copy the feature store from another. The preferred method of moving data is
+    // with swap.
+    void Copy(const FeatureDescriptor &other)
+    {
+        num_features_ = other.num_features_;
+        descriptor_size_ = other.descriptor_size_;
+        descriptor_data_ = other.descriptor_data_;
+    }
+
+private:
+    // Prevent accidental use of the copy constructor since it is expensive.
+    // The preferred way to move feature stores around is with swap().
+    FeatureDescriptor(const FeatureDescriptor &) = delete;
+    FeatureDescriptor &operator=(const FeatureDescriptor &) = delete;
+
+    // The number of features in the store.
+    uint32_t num_features_;
+
+    // Number of bytes per descriptor.
+    uint32_t descriptor_size_;
+
+    std::vector<ScalarType> descriptor_data_;
+};
+
+using FeatureDescriptoru = FeatureDescriptor<unsigned char>;
+using FeatureDescriptorf = FeatureDescriptor<float>;
+
+#endif
---
+++ thirdparty/feature_tracker/include/SuperPointFeatureMatcher.h
@@ -0,0 +1,60 @@
+#ifndef SUPER_POINT_FEATURE_MATCHER_H
+#define SUPER_POINT_FEATURE_MATCHER_H
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <vector>
+
+#include "FeatureDescriptor.h"
+
+class SuperPointFeatureMatcher
+{
+public:
+    /* Feature matched indices. */
+    struct FeatureMatch
+    {
+        int index1;
+        int index2;
+        float distance;
+        FeatureMatch(int idx1, int idx2, float dist)
+            : index1(idx1), index2(idx2), distance(dist){};
+        FeatureMatch()
+            : index1(-1), index2(-1), distance(std::numeric_limits<float>::max()){};
+    };
+
+    SuperPointFeatureMatcher();
+
+    ~SuperPointFeatureMatcher() = default;
+
+    void Init();
+
+    void Init(float nn_thresh);
+
+    bool IsInitialized() const { return is_initialized_; }
+
+    SuperPointFeatureMatcher(const SuperPointFeatureMatcher &d);
+
+    SuperPointFeatureMatcher &operator=(const SuperPointFeatureMatcher &d);
+
+    int Match(const FeatureDescriptorf &descriptors_img1,
+              const FeatureDescriptorf &descriptors_img2,
+              Eigen::Matrix3Xf *output_matches);
+
+    inline float GetRefineDist() const { return refine_dist_; }
+
+    std::vector<FeatureMatch> &GetMatches() { return matches_; }
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+private:
+    int Match(const FeatureDescriptorf &descriptors_img1,
+              const FeatureDescriptorf &descriptors_img2);
+    float refine_dist_;
+    bool is_initialized_;
+    std::vector<FeatureMatch> matches_;
+};
+
+#endif
